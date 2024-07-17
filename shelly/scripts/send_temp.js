8e946ec58c3997b3b679cd8e4aa03d83e39411a4
Teppo Jalava <tjjalava@iki.fi>
2024-07-18 00:59:32 +0300

    send_temp

--- shelly/scripts/send_temp.js
+++ shelly/scripts/send_temp.js
@@ -1,3 +1,40 @@
-let tempYla = Shelly.getComponentStatus("temperature:100")
-let tempAla = Shelly.getComponentStatus("temperature:101")
-print("Up temp: ", tempYla, " Down temp: ", tempAla)
+let token = "secret_token"
+let url = "https://temperature2"
+
+function sendTemp() {
+  let compYla = Shelly.getComponentStatus("temperature:100")
+  let compAla = Shelly.getComponentStatus("temperature:101")
+  let body = []
+
+  if (compYla === null && compAla === null) {
+    print("No temperature sensors found")
+    return
+  }
+
+  if (compYla != null) {
+    body.push({"peripheral": compYla.id, "temperature": compYla.tC})
+  }
+
+  if (compAla != null) {
+    body.push({"peripheral": compAla.id, "temperature": compAla.tC})
+  }
+
+  Shelly.call(
+    "HTTP.Request", {
+      url: url,
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + token
+      },
+      body: JSON.stringify(body)
+    }, function(result, error, errorMessage) {
+      if (error) {
+        print("Error: ", errorMessage)
+      } else {
+        print("Temperatures sent")
+      }
+    }
+  )
+}
+
+Timer.set(60000, true, sendTemp)
